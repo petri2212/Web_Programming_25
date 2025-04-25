f40fdbac62a8844d637510043d085634ae55fe9c
Andrei <a.petrisor@studenti.unibg.it>
2025-04-25 15:23:21 +0200

    move

---
+++ queries/select.php
@@ -0,0 +1,29 @@
+<?php
+require_once('config.php');
+
+$descrizione = $connessione ->real_escape_string($_POST['Descrizione']);
+
+$sql = "SELECT * FROM tema WHERE descrizione LIKE '%" . $descrizione ."%' ";//WHERE descrizione = "Arte classica" WHERE  descrizione = $descrizione
+
+if ($result = $connessione->query($sql)) {
+    $data = [];
+    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+        $tep;
+        $tmp['codice'] = $row['codice'];
+        $tmp['descrizione'] = $row['descrizione'];
+        array_push($data, $tmp);
+
+    }
+    echo json_encode($data);
+}
+
+
+
+
+
+
+
+
+
+
+?>
