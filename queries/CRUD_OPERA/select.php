b91411688df2656a3228d6cb564e12dc51362adf
zodiacapricorn <a.radu@studenti.unibg.it>
2025-04-29 10:13:59 +0200

    update

---
+++ queries/CRUD_OPERA/select.php
@@ -0,0 +1,20 @@
+<?php
+//SELECT
+/**
+ * 1) Messaggi per far capire cosa inserire
+ * 2) Bisogna gestire il fatto che se l'autore non esiste, bisogna poterlo creare --> "nome", "cognome", "nazione", "data di nascita", "data morte". Se vivo o morto lo definiamo in base alla "data morte".
+ * 3) Inserimento dell'utente solo di: "autore", "titolo", "anno di acquisto", "anno di realizzazione", "tipologia", "numero sala"
+ * 4) Bisogna gestire la selezione della sala --> va fatta una query per pescare tutte le sale per poterle metterle all intenrno di una lista (select)
+ * 
+ */
+
+//READ
+/**
+ * 1)
+ * 
+ * 
+ * 
+ */
+
+
+?>
